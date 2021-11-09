f8f0d3f4aa4ffbcb20525eac4dcc7f25e0ce4bd2
augustoroocha <augustinho_darocha1311@hotmail.com>
2021-11-08 22:11:44 -0200

    configurando a aplicação

--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>This is an about {{$route.params.id}}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+}
+</script>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main>
+    <div class="filmesPopulares">
+      <ul v-for="filmePopular in filmesPopulares" :key="filmePopular.id">
+        <li>
+          {{ filmePopular.adult }}
+          {{ filmePopular.popularity }}
+          {{ filmePopular.title }}
+          {{ filmePopular.original_title }}
+          {{ filmePopular.original_language }}
+          {{ filmePopular.overview }}
+        </li>
+        <img
+          :src="filmePopular.poster_path"
+          height="40"
+          width="40"
+          alt="error"
+        />
+      <router-link :to="{ name: 'About', params: { id: filmePopular.id } }"
+        >ℹ️</router-link
+      >
+      </ul>
+    </div>
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  data() {
+    return {
+      filmesPopulares: [],
+    };
+  },
+  async mounted() {
+    await axios
+      .get(
+        "https://api.themoviedb.org/3/movie/popular?api_key=e3c3595204a142ec627688c9ea2ad00f&language=en-US&page=1"
+      )
+      .then((response) => {
+        response.data;
+        this.filmesPopulares = response.data.results;
+        console.log(response.data);
+      });
+  },
+};
 </script>
